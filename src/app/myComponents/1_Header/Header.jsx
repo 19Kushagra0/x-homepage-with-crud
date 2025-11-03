2d8fcd6702a55cbaf3439e7a23fc049395e7fedb
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-03 18:06:58 +0530

    install nextJs

---
+++ src/app/myComponents/1_Header/Header.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "@/app/myComponents/1_Header/Header.css";
+
+export default function Header() {
+  return <div className="app"></div>;
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Header from "@/app/myComponents/1_Header/Header";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Header />
+    </div>
+  );
+}
